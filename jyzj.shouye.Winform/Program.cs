0ae23396a3b939105908df72fe99e3755e512588
祝威 <bitzhuwei.cnblogs@qq.com>
2017-01-25 17:15:14 +0800

    update

--- jyzj.shouye.Winform/Program.cs
+++ jyzj.shouye.Winform/Program.cs
@@ -16,7 +16,7 @@ namespace jyzj.shouye.Winform
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormMain());
         }
     }
 }
