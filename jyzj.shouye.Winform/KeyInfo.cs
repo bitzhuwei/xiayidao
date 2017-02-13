4ad405e465c68922b292672cd4d31dbe704d26fd
祝威 <bitzhuwei.cnblogs@qq.com>
2017-02-13 14:23:24 +0800

    update

--- jyzj.shouye.Winform/KeyInfo.cs
+++ jyzj.shouye.Winform/KeyInfo.cs
@@ -19,8 +19,32 @@ namespace jyzj.shouye.Winform
 
         public override string ToString()
         {
-            return this.key.ToString();
+            return string.Format("{0}({1})", this.key, Translate(this.key));
+        }
+
+        private string Translate(System.Windows.Forms.Keys key)
+        {
+            var result = string.Empty;
+            switch (key)
+            {
+                case System.Windows.Forms.Keys.A:
+                    result = "←";
+                    break;
+                case System.Windows.Forms.Keys.S:
+                    result = "↓";
+                    break;
+                case System.Windows.Forms.Keys.D:
+                    result = "→";
+                    break;
+                case System.Windows.Forms.Keys.W:
+                    result = "↑";
+                    break;
+                default:
+                    result = key.ToString();
+                    break;
+            }
+            return result;
         }
     }
-
 }
+
